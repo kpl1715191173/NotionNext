974bc0bcf815d7d66d1747546b2f0a9c1f3710a3
Alan Lee <1715191173@qq.com>
2024-07-23 09:39:21 +0800

    chore: heo主题归档图片问题

--- themes/heo/components/BlogPostArchive.js
+++ themes/heo/components/BlogPostArchive.js
@@ -43,10 +43,10 @@ const BlogPostArchive = ({ posts = [], archiveTitle, siteInfo }) => {
                 }>
                 {/* 图片封面 */}
                 {showPageCover && (
-                  <div>
+                  <div className={'w-40 h-24'}>
                     <Link href={post?.href} passHref legacyBehavior>
                       <LazyImage
-                        className={'rounded-xl bg-center bg-cover w-40 h-24'}
+                        className={'rounded-xl bg-center bg-cover object-cover'}
                         src={post?.pageCoverThumbnail}
                       />
                     </Link>
